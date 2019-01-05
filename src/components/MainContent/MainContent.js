fb80ee2f15268a22bfc4ec082315771af7f194e7
selistym <selistym@protonmail.com>
2019-01-05 01:19:14 -0800

    update maincontent.js

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import {
+  MDBRow,
+  MDBCol,
   Navbar,
   NavbarBrand,
   NavbarNav,
   NavbarToggler,
   Collapse,
-  NavItem,  
+  NavItem,
   NavLink
 } from "mdbreact";
 
@@ -40,110 +42,109 @@ class MainContent extends Component {
       />
     );
     return (
-        <Router>
-            <div className="flyout">
-            <Navbar color="white" expand="md" scrolling className="navbar">
-              <NavbarBrand className="mr-5" href="/">
-                <img src={logoImg} alt="logo" height="40" />
-                {" "}
-              </NavbarBrand>
-              <NavbarToggler
-                onClick={this.toggleCollapse("mainNavbarCollapse")}
-              />
-              <Collapse
-                className="ml-5"
-                id="mainNavbarCollapse"
-                isOpen={this.state.collapseID}
-                navbar
-              >
-                <NavbarNav center="true">
-                  <NavItem>
-                    <NavLink
-                      exact
-                      to="/"
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                    >
-                      Dashboard
+      <Router>
+        <div className="flyout">
+          <Navbar color="white" expand="md" scrolling className="navbar">
+            <MDBRow >
+              <MDBCol md="4">
+                <NavbarBrand href="/">
+                  <img src={logoImg} alt="logo" height="40" />
+                  {" "}
+                </NavbarBrand>
+                <NavbarToggler
+                  onClick={this.toggleCollapse("mainNavbarCollapse")}
+                />
+              </MDBCol>
+              <MDBCol md="8">
+                <Collapse
+                  id="mainNavbarCollapse"
+                  isOpen={this.state.collapseID}
+                  navbar
+                >
+                  <NavbarNav center="true">
+                    <NavItem>
+                      <NavLink
+                        exact
+                        to="/"
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                      >
+                        Dashboard
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/hotels"
-                    >
-                      Hotels
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/hotels"
+                      >
+                        Hotels
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/components"
-                    >
-                      Transfers
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/components"
+                      >
+                        Transfers
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/advanced"
-                    >
-                      My Bookings
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/advanced"
+                      >
+                        My Bookings
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/"
-                    >
-                      My Company
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/"
+                      >
+                        My Company
                     </NavLink>
-                  </NavItem>
-                </NavbarNav>
-              </Collapse>
-              <Collapse
-                id="mainNavbarCollapse"
-                isOpen={this.state.collapseID}
-                navbar
-              >
-                <NavbarNav right>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/"
-                    >
-                      News
+                    </NavItem>
+                  </NavbarNav>
+                  <NavbarNav right>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/"
+                      >
+                        News
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/"
-                    >
-                      Help
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/"
+                      >
+                        Help
                     </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      onClick={this.closeCollapse("mainNavbarCollapse")}
-                      to="/"
-                    >
-                      Contacts
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        onClick={this.closeCollapse("mainNavbarCollapse")}
+                        to="/"
+                      >
+                        Contacts
                     </NavLink>
-                  </NavItem>                
-                </NavbarNav>
-              </Collapse>
-            </Navbar >          
-            {this.state.collapseID && overlay}
-            <main className="d-flex flex-row">
-              <div className="leftbar-pane flex-shrink-1">
-                <Leftbar />
-              </div>
-              <div className="main-pane w-100">
-                <Routes />
-              </div>              
-            </main >            
-          </div>
-        </Router >      
+                    </NavItem>
+                  </NavbarNav>
+                </Collapse>
+              </MDBCol>
+            </MDBRow>
+          </Navbar >
+          {this.state.collapseID && overlay}
+          <main className="d-flex flex-row">
+            <div className="leftbar-pane flex-shrink-1">
+              <Leftbar />
+            </div>
+            <div className="main-pane w-100">
+              <Routes />
+            </div>
+          </main >
+        </div>
+      </Router >
     );
   }
 }
